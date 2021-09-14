84e1a91f4d78aee15fc5b0263b197c2f5a07e137
Alin <90528858+Alininumi@users.noreply.github.com>
2021-09-14 23:56:39 +0200

    Ejercicio 1

---
+++ Ejercicio_1/src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,41 @@
+package ejercicio1;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+	
+	public int numUno= 0;
+	public int numDos= 0;
+
+	public Ejercicio1() {	
+	}
+	
+	public int suma() {
+		
+		return numUno+numDos;
+		
+	}
+
+	
+	public static void main(String[] args) {
+		
+		try (Scanner input = new Scanner(System.in)) {
+			
+			Ejercicio1 valores= new Ejercicio1();
+			
+			
+			System.out.print("Introduce los dos numeros para sumarlos..");
+			
+			valores.numUno=input.nextInt(); 
+			valores.numDos=input.nextInt();
+			
+			System.out.print(valores.suma());
+		}
+		catch(Exception e) {
+			System.out.print("Error");
+		}
+		
+		
+	}
+
+}
